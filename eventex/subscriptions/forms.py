4f02045d12650ba034ccb750f6bfa49c13a84e2a
Marcos <mvmello27@gmail.com>
2018-08-20 14:47:52 -0300

    Email ou telefone opicionais

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -4,19 +4,24 @@ from django.core.exceptions import ValidationError
 
 def validate_cpf(value):
     if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números', 'digits')
+        raise ValidationError('CPF deve conter apenas números.', 'digits')
     if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 números', 'length')
+        raise ValidationError('CPF deve ter 11 números.', 'length')
 
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
 
     def clean_name(self):
         name = self.cleaned_data['name']
         words = [w.capitalize() for w in name.split()]
         return ' '.join(words)
 
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone')
+
+        return self.cleaned_data
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
-
 class SubscriptionFormTest(TestCase):
     def test_form_has_fields(self):
         """Form must have 4 fields"""
@@ -10,12 +9,12 @@ class SubscriptionFormTest(TestCase):
         self.assertSequenceEqual(expected, list(form.fields))
 
     def test_cpf_is_digit(self):
-        """CPF MUST ONLY ACCEPT DIGITS"""
-        form = self.make_validated_form(cpf='ABCD5678901')
+        """CPF must only accept digits"""
+        form = self.make_validated_form(cpf='ABCD567890')
         self.assertFormErrorCode(form, 'cpf', 'digits')
 
     def test_cpf_has_11_digits(self):
-        """CPF MUST HAVE 11 DIGITS"""
+        """CPF must have 11 digits"""
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
@@ -24,24 +23,36 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(name='MARCOS santos')
         self.assertEqual('Marcos Santos', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed"""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
         exception = errors_list[0]
-        self.assertEqual(code , exception.code)
+        self.assertEqual(code, exception.code)
 
     def assertFormErrorMessage(self, form, field, msg):
         errors = form.errors
         errors_list = errors[field]
         self.assertListEqual([msg], errors_list)
 
-
     def make_validated_form(self, **kwargs):
-        valid = dict(name='Marcos Santos', cpf='1234',
+        valid = dict(name='Marcos Santos', cpf='12345678901',
                     email='mvmello27@gmail.com', phone='21-12345-6789')
         data = dict(valid, **kwargs)
         form = SubscriptionForm(data)
         form.is_valid()
         return form
-
